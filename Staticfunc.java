6e6ee4be566b3486aab50093c2de3536a967dbdb
sandyashu17 <37293278+sandyashu17@users.noreply.github.com>
2018-03-14 16:26:29 +0530

    Add files via upload

---
+++ Staticfunc.java
@@ -0,0 +1,13 @@
+class Staticfunc
+{
+static int a=5;
+public static void main(String args[])
+{
+display();                 
+} 
+  public static void display()
+{
+int a=5;
+System.out.println(a);
+}
+}
