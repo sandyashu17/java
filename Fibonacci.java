2883585921e06c0bf7165ccd9d4df9b1c3141245
sandyashu17 <37293278+sandyashu17@users.noreply.github.com>
2018-03-19 22:09:18 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Fibonacci
+{
+public static void main (String []args)
+{
+int n1=0,n2=1,n3,value=10;
+System.out.print(n1+" "+n2);
+for (int i=2;i<value;i++)
+{
+n3=n1+n2;
+System.out.print(" "+n3);
+n1=n2;
+n2=n3;
+}
+}
+}
