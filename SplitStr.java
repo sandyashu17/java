ce9167f925fc286246ba12db59ab38c54b3a41ea
sandyashu17 <37293278+sandyashu17@users.noreply.github.com>
2018-03-23 17:00:41 +0530

    Add files via upload

---
+++ SplitStr.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class SplitStr
+{
+public static void main (String []args)
+{
+String s="Hi Good Morning";
+String[] a=s.split(" ");
+for(String b:a)
+System.out.println(b);
+}
+}
